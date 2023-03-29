c7e0d65923f9489cf6fc1d943e7ce44765b2d14b
amberwagoner <amberwagoner@outlook.com>
2023-03-29 12:34:42 -0700

    updated state

--- models/state.py
+++ models/state.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.city import City
-from os import getenv
 import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
-    else:
-        @property
-        def cities(self):
-            """placeholder"""
-            city_list = []
-            for el in models.storage.all(City).values():
-                if el.state_id == self.id:
-                    city_list.append(el)
-            return city_list
+if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+
+    class State(BaseModel, Base):
+        """ State class """
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="delete")
+else:
+    class State(BaseModel):
+        """
+        State class
+        """
+        name = ''
+
+    @property
+    def cities(self):
+        """
+        getter method, returns
+        list of City objs from storage linked to the current State
+        """
+        city_list = []
+        for city in models.storage.all("City").values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
